eb21612e06c4cc7da1b18b7cf3ac0de90c3ca315
Edgar Daniel <danichg94@gmail.com>
2018-02-19 13:34:21 -0600

    Algoritmo funcinando

--- Quiz1.cpp
+++ Quiz1.cpp
@@ -44,7 +44,7 @@ bool Trabajador::is_valid(){ // condiones para validar
 
 // Funcion que llama la Clase y valida
 void PotsWork(int liters){
-    int pot1 = 11, pot2 = 5 ;
+    int pot1 = 9, pot2 = 4 ;
     try{
         Trabajador Pots {pot1, pot2} ;
 
@@ -78,20 +78,26 @@ void PotsWork(int liters){
             cout << "\n" ;
         }
 
-        if(multiplo == false){
+        if(multiplo == false && pot1>=liters){
             for(i=0; i<=pot1; i++){
                 if((pot1-pot2*i) < liters){  // 6, 7 litros
                     // +BaseMayor
-                    Pots.llene(pot1) ;
-                    for(int f=0; f<(pot1-pot2*(i+1)); f++){
+                    //int h ;
+                    //for(int e=0; e<i; e++){  // Base +5 y +1
+                        Pots.llene(pot1) ;
                         Pots.llene_con(pot1, pot2) ;
                         Pots.bote(pot2) ;
-                    }
-                    Pots.llene_con(pot1, 0) ;
-                    cout << ", +" << pot1-pot2*i << "\n\n" ;
+                        for(int h=1; h<i; h++){
+                            Pots.llene_con(pot1, pot2) ;
+                            Pots.bote(pot2) ;
+                        }
+                        Pots.llene_con(pot1, 0) ;
+                        cout << ", +" << pot1-pot2*(i) << "\n\n" ;
+                        //break ;
+                    //}
 
                     // +1 litros
-                    for(int g=0; g<(liters-(pot1-pot2*i)); g++){
+                    for(int g=0; g<(liters-(pot1-pot2*(i))); g++){
                         Pots.llene(pot1) ;
                         for(int h=0; h<toOne; h++){
                             Pots.llene_con(pot1, pot2) ;
@@ -112,6 +118,50 @@ void PotsWork(int liters){
                     cout << ", +" << pot1-pot2*i << "\n" ;
                     break ;
                 }
+
+            }
+        }
+
+        else if(multiplo == false && pot1<liters){
+            for(i=0; i<pot1; i++){
+                if((pot1+pot2*i) == liters){  // 13 y 17 litros
+                    for(int m=0; m<i; m++){
+                        Pots.llene(pot1) ;
+                        Pots.llene_con(pot1, 0) ;
+                        for(int f=0; f<(liters-pot1)/pot2; f++){
+                            Pots.llene(pot2) ;
+                            Pots.llene_con(pot2, 0);
+                        }
+                        cout << ", +" << pot1+pot2*i << "\n\n" ;
+                        break ;
+                    }
+                    break ;
+                }
+                else if((pot1+pot2*i) > liters){  // (Base + 1, 2 ..), (10, 11, 12...)
+                    int m, f ;
+                    for(m=0; m<i; m++){ // +Base  +9
+                        Pots.llene(pot1) ;
+                        Pots.llene_con(pot1, 0) ;
+                        for(f=0; f<liters-(pot1+pot2*(f+1)); f++){  // +4
+                            Pots.llene(pot2) ;
+                            Pots.llene_con(pot2, 0) ;
+                        }
+                        cout << ", +" << pot1*(m+1)+pot2*(f) << "\n\n" ;
+                        break ; // ponerle condicion al break para mayores a 18
+                    }
+                    // +1
+                    for(int l=0; l<(liters-(pot1*(m+1)+pot2*f)); l++){ // 10, 11
+                        Pots.llene(pot1) ;
+                        Pots.llene_con(pot1, pot2) ;
+                        Pots.bote(pot2) ;
+                        Pots.llene_con(pot1, pot2) ;
+                        Pots.bote(pot2) ;
+                        Pots.llene_con(pot1, 0) ;
+                        cout << ", +1" << "\n\n" ;
+                    }
+                    //cout << ", +" << pot1+liters-pot1*(m+1) << "\n\n" ;
+                    break ;
+                }
             }
         }
 /* ---------------------------------------------------------------- */
@@ -126,7 +176,49 @@ void PotsWork(int liters){
 int main()
 {
     // Funcion que llama la clase Trabajador
-    PotsWork(7) ;
+    PotsWork(21) ;
 
     return 0 ;
 }
+
+                    //cout << ", +" << pot1+pot2*i << "\n" ;
+/*if((pot1+pot2*i) < liters){  // 7, 8 litros
+    // +BaseMayor
+    Pots.llene(pot1) ;
+    for(int f=0; f<(pot1+pot2*(i+1)); f++){
+        Pots.llene_con(pot1, pot2) ;
+        Pots.bote(pot2) ;
+    }
+    Pots.llene_con(pot1, 0) ;
+    cout << ", +" << pot1+pot2*i << "\n\n" ;
+
+    // +1 litros
+    for(int g=0; g<(liters-(pot1-pot2*i)); g++){
+        Pots.llene(pot1) ;
+        for(int h=0; h<toOne; h++){
+            Pots.llene_con(pot1, pot2) ;
+            Pots.bote(pot2) ;
+        }
+        Pots.llene_con(pot1, 0) ;
+        cout << ", +1 \n" ;
+    }
+    break ;
+}
+else*/
+/*Pots.llene(pot1) ;
+Pots.llene(pot2) ;
+Pots.llene_con(pot1, 0) ;
+Pots.llene_con(pot2, 0) ;
+//Pots.llene_con(pot1, pot2) ;
+//Pots.bote(pot2) ;*/
+/*
+// +1 litros
+for(int g=0; g<(liters-(pot1-pot2*i)); g++){
+    Pots.llene(pot1) ;
+    for(int h=0; h<toOne; h++){
+        Pots.llene_con(pot1, pot2) ;
+        Pots.bote(pot2) ;
+    }
+    Pots.llene_con(pot1, 0) ;
+    cout << ", +1 \n" ;
+}*/
