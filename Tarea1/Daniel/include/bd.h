9c502531f7f0c24076dddc91491e71aaed8e1018
Edgar Daniel <danichg94@gmail.com>
2018-03-23 11:43:58 -0600

    Update bd.h

--- Tarea1/Daniel/include/bd.h
+++ Tarea1/Daniel/include/bd.h
@@ -1,3 +1,28 @@
+/*
+******************************************************
+* Instituto Tecnológico de Costa Rica.
+* Ingeniería en Computación
+* Curso     : IC-2001 Grupo 2
+* Proyecto  : BD.pro
+* Archivo   : bd.h
+* Autor     : Edgar Daniel Chaves
+* Carnet    : 2013095110
+* Fecha     : 21 de Marzo de 2018
+*
+======================== Contenido ==================
+*
+* Mantiene al definición e implementación de las
+* siguientes clases:
+* >> Nodo
+* >> Book
+* >> MStorage
+* >> FileReader
+* >> Pqueue
+* >> ISBN
+*
+*/
+
+
 #ifndef BD_H
 #define BD_H
 
@@ -12,30 +37,110 @@
 
 using namespace std ;
 
-
-///////////////// Nodo ////////////////////
+////////////////////////////////////////////////////
+///             Clase Nodo
+////////////////////////////////////////////////////
 class Nodo {
+    /* *********************************************
+    * La clase Nodo permite crear un registro que
+    * contiene elementos o datos de interés, y a su
+    * vez ofrece uno o dos punteros para referenciar
+    * (apuntar) a otro nodo.
+    *
+    * Ejemplo de uso:
+    *
+    * pdato = NULL ;
+    * pdato = new Nodo(elem, pdato) ;
+    *
+    * En el ejemplo anterior se inserta
+    * el dato "elem" en el Nodo, el cual tiene
+    * asociado el puntero "pdato".
+    *
+    * */
 public:
+    // Registros que soporta el Nodo
     string isbn, titulo, autor, year, cantidad ;
     string user ;
+    // Puntero al siguiente Nodo
     Nodo *siguiente ;
-/*--------------------------- Sobrecarga para  Clase Queue ----------------------------------------------*/
+
     Nodo(string, string) ;
     Nodo(string, string, Nodo *) ;
+    /* =====================================================
+        * Constructor con sobrecarga para la Clase Pqueue
+        *
+        * Entradas
+        *        isbn   : ISBN del libro solicitado
+        *        user   : Nombre del usuario
+        *        pdato  : Puntero para referenciar el Nodo
+        * Salidas
+        *        Instancia de la Clase Nodo.
+        * Restricciones
+        *        Recibe tipo string.
+    * ================================================ */
+
+    // Punteros al frente y final para Lista circular doble
     Nodo *frente ;
     Nodo *final ;
-/*---------------------------- Sobrecarga para  Clase Book ----------------------------------------------*/
+
     Nodo(string, string, string, string, string) ;
     Nodo(string, string, string, string, string, Nodo *) ;
+    /* =============================================================
+        * Constructor con sobrecarga para la Clase Book
+        *
+        * Entradas
+        *        isbn     : ISBN del libro
+        *        titulo   : Titulo del libro
+        *        autor    : Autor del libro
+        *        año      : Año de publicación del libro
+        *        cantidad : Cantidad de ejemplares en la Biblioteca
+        *        pdato    : Recibe un puntero al Nodo para referenciar
+        * Salidas
+        *        Instancia de la Clase Nodo.
+        * Restricciones
+        *        Recibe tipo string.
+    * ============================================================== */
 } ;
 
-///////////////// Book ////////////////////
+////////////////////////////////////////////////////
+///             Clase Book
+////////////////////////////////////////////////////
 class Book {
+    /* *********************************************
+    * La clase Book permite crear el objeto libro,
+    * que mantiene la información de un libro.
+    *
+    * Ejemplo de uso:
+    *
+    * Book book = Book(ISBN, Titulo, Autor,
+    *                 Año, Cantidad) ;
+    *
+    * En el ejemplo anterior se crea un nuevo libro.
+    *
+    * */
+
+    // Puntero a Nodo para Registrar información
     Nodo *pdato ;
+    // Parámetros de cada libro
     string isbnB ="", tituloB, autorB, yearB, cantidadB ;
 public:
     Book() ;
     Book(string, string, string, string, string) ;
+    /* =====================================================
+        * Constructor con sobrecarga
+        *
+        * Entradas
+        *        ISBN     : ISBN del libro
+        *        Titulo   : Nombre del libro
+        *        Autor    : Nombre del autor del libro
+        *        Año      : Año de publicación del libro
+        *        Cantidad : Cantidad de libros en Biblioteca
+        * Salidas
+        *        Instancia de la Clase Book
+        * Restricciones
+        *        Datos deben ser coherentes
+    * ================================================ */
+
     void insertar(string, string, string, string, string) ;
     void mostrar() ;
     string buffering() ;
@@ -49,7 +154,9 @@ public:
 
 } ;
 
-/////////////// MStorage //////////////////
+////////////////////////////////////////////////////
+///             Clase MStorage
+////////////////////////////////////////////////////
 class MStorage:public Book {
     string isbn, titulo, autor, year, cantidad, content ;
 public:
@@ -74,8 +181,9 @@ public:
 
 } ;
 
-
-/////////////// File Reader ///////////////////////
+////////////////////////////////////////////////////
+///             Clase FileReader
+////////////////////////////////////////////////////
 class FileReader {
 private:
     const char *BD_file ;
@@ -89,7 +197,9 @@ public:
 
 } ;
 
-////////////////// Pqueue /////////////////////////
+////////////////////////////////////////////////////
+///             Clase Queue
+////////////////////////////////////////////////////
 class Queue:public MStorage{
     Nodo *pdato ;
     Nodo *frente ;
@@ -108,10 +218,13 @@ public:
 
 } ;
 
-/////////////////// ISBN //////////////////////////
+////////////////////////////////////////////////////
+///             Clase ISBN
+////////////////////////////////////////////////////
 class ISBN {
 private:
-    int pref, reg, edit, elem, verif ;
+    //int pref, reg, edit, elem, verif ;
+    int pr, pa, ed, sec, dv ;
     string ss="", isbnInit, fullIsbn ;
     // estructura para los parámetros
     struct ISBNparams{
@@ -127,11 +240,9 @@ public:
     int nn; // cantidad de numeros entre todos los parametros
 
     ISBN() ;
-    ISBN(string) ;
-    // ISBN 13 digitos
-    ISBN(int p, int r, int e, int el) ;
-    // ISBN 10 digitos
-    ISBN(int r, int e, int el) ;
+    ISBN(string) ;    
+    ISBN(int, int, int, int, int) ; // ISBN 13 digitos
+    ISBN(int, int, int, int) ;      // ISBN 10 digitos
 
     string wholeIsbn() ;
     string getFullIsbn(string) ;
