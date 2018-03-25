4509055954fdf062604dd9486970f4a52f831973
Edgar Daniel <danichg94@gmail.com>
2018-03-25 02:27:53 -0600

    Add files via upload

---
+++ Tarea1/Daniel/include/bd.h
@@ -0,0 +1,10 @@
+
+
+
+#ifndef BD_H
+#define BD_H
+
+
+
+#endif // BD_h
+
---
+++ Tarea1/Daniel/include/filewriter.h
@@ -0,0 +1,46 @@
+#ifndef FILEWRITER_H
+#define FILEWRITER_H
+
+#include <iostream>
+#include <fstream>  // writer
+//#include <ofstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <stdlib.h>
+#include <vector>
+
+#include "mstorage.h"
+//#include "filewriter.h"
+
+using namespace std ;
+
+////////////////////////////////////////////////////
+///             Clase FileWriter
+////////////////////////////////////////////////////
+class FileWriter:public MStorage {
+private:
+    const char *BD_file1 ;
+    //MStorage mStorage ;
+    //string isbn, titulo, autor, year, cantidad, content ;
+    //string buffer, nextBookBuf ;
+public:
+    FileWriter():MStorage(){}
+    FileWriter(const char *) ;
+    FileWriter(const char *, MStorage &) ;
+/*    FileWriter(const char *file, MStorage &mStorage):MStorage(){
+        BD_file1 = file ;
+        //MStorage mStorage ; //= mStorage1 ;
+        cout << "testX= " << mStorage.getAutorMS() << endl ;
+    }
+*/
+    //FileWriter(const char *, MStorage &):MStorage(){}
+    void fwriter(string) ;
+    bool empty() ;
+    //string getBuffer() ;
+    //string next() ;
+    //string untilTokenFR(string &) ;
+
+} ;
+
+#endif // FILEWRITER_H
